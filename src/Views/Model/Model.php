388fdd0329cf8a35261218645f4a0d1c93470803
Soft solutions <104563618+SIENSIS@users.noreply.github.com>
2025-02-25 15:42:49 +0100

    Add to model template CI4 fields as comment

--- src/Views/Model/Model.php
+++ src/Views/Model/Model.php
@@ -9,21 +9,39 @@ class {! name !} extends Model
 {
     protected $table      = '{! table !}';
     protected $primaryKey = '{! primaryKey !}';
-
-    protected $protectFields    = true;
-
-    protected $allowedFields = [{! allowedFields !}];
-
+    //    protected $useAutoIncrement = true;
     protected $returnType = '{! returnType !}';
     protected $useSoftDeletes = {! useSoftDeletes !};
+    protected $protectFields    = true;
+    protected $allowedFields = [{! allowedFields !}];
 
+    // protected bool $allowEmptyInserts = false;
+    // protected bool $updateOnlyChanged = true;
+
+    // protected array $casts = [];
+    // protected array $castHandlers = [];
+
+    // Dates
     protected $useTimestamps = {! useTimestamps !};
     protected $createdField  = '{! createdField !}';
     protected $updatedField  = '{! updatedField !}';
     protected $deletedField  = 'deleted_at';
     protected $dateFormat    = '{! dateFormat !}';
 
+    // Validation
     protected $validationRules    = {! validationRules !};
     protected $validationMessages = [];
     protected $skipValidation     = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    // protected $allowCallbacks = true;
+    // protected $beforeInsert   = [];
+    // protected $afterInsert    = [];
+    // protected $beforeUpdate   = [];
+    // protected $afterUpdate    = [];
+    // protected $beforeFind     = [];
+    // protected $afterFind      = [];
+    // protected $beforeDelete   = [];
+    // protected $afterDelete    = [];
 }
